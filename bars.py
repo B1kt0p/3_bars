7cbede34861cb172c57a6671eb19adb6ee37426d
buktop <buktop>
2018-01-12 01:41:42 +0900

    Исправлены ошибки

--- bars.py
+++ bars.py
@@ -19,8 +19,8 @@ def parser_argv():
 def load_data(file_path):
     try:
         with open(file_path, "r", encoding="utf-8") as f:
-            data = f.read()
-            return json.loads(data)
+            json_content = f.read()
+            return json.loads(json_content)
     except FileNotFoundError:
         return None
     except AttributeError:
@@ -29,33 +29,20 @@ def load_data(file_path):
 
 def get_biggest_bar(json_content):
     try:
-        sort_bars = sorted(json_content['features'],
+        biggest_bar = max(json_content['features'],
                            key=lambda x:
                            x['properties']['Attributes']['SeatsCount'])
-        small_bars = []
-        x = 0
-        for bars in sort_bars[:10]:
-            bar_zip = zip(bars['properties']['Attributes'].keys(),
-                          bars['properties']['Attributes'].values())
-            small_bars.append(dict(bar_zip))
-        return small_bars
+        return biggest_bar['properties']['Attributes']
     except IndexError:
         return None
 
 
 def get_smallest_bar(json_content):
     try:
-        sort_bars = sorted(json_content['features'],
+        small_bars = min(json_content['features'],
                            key=lambda x:
-                           x['properties']['Attributes']['SeatsCount'],
-                           reverse=True)
-        small_bars = []
-        x = 0
-        for bars in sort_bars[:10]:
-            bar_zip = zip(bars['properties']['Attributes'].keys(),
-                          bars['properties']['Attributes'].values())
-            small_bars.append(dict(bar_zip))
-        return small_bars
+                           x['properties']['Attributes']['SeatsCount'])
+        return small_bars['properties']['Attributes']
     except IndexError:
         return None
 
@@ -88,9 +75,7 @@ def get_closest_bar(json_content, longitude, latitude):
                                                       coordinate_bar[0]))))
                 distance_bars.append(distance)
                 index_min_distance_bar = distance_bars.index(min(distance_bars))
-            return json_content['features'][index_min_distance_bar]
-            ['properties']['Attributes'], \
-            min_distance_bar
+            return json_content['features'][index_min_distance_bar]['properties']['Attributes'], min(distance_bars)
         else:
             return None
     except IndexError:
@@ -104,9 +89,9 @@ if __name__ == '__main__':
         smallest_bar = get_smallest_bar(json_content)
         biggest_bar = get_biggest_bar(json_content)
         if smallest_bar and biggest_bar:
-            print("\n 10 самых маленьких баров:")
+            print("\n Самый маленький бар:")
             pprint.pprint(smallest_bar)
-            print("\n10 самых больших баров:")
+            print("\n Cамый большой бар:")
             pprint.pprint(biggest_bar)
         else:
             print("Не могу прочитать. Неправильный формат данных")
